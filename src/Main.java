6a069d5fdf1105d382f232ddd3b20637a3f3ee2e
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-04-12 22:25:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int bonus = ticketPrice / 20;
+
+        System.out.println("Начисленные мили: " + bonus);
+
+    }
+
+}
