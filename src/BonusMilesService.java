cfeade2b8b37d0ff52ea36e999ff84cd23bc96fc
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-05-04 11:15:45 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,3 @@
-import static jdk.internal.logger.LoggerFinderLoader.service;
-
 public class BonusMilesService {
     public int calculate(int price) {
         int miles = price / 20;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int miles = service.calculate(price = 10000);
+        int price = 10_000;
+        int miles = service.calculate( price = 10000);
         System.out.println("Начисленные мили = " + miles);
 
     }
